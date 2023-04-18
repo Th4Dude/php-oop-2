c687584ccada790763bef882607b9f4ccdb204dc
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-18 18:58:12 +0300

    Trait Fatto

--- prodotto.php
+++ prodotto.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/trait.php';
 require_once __DIR__.'/exception.php';
 
 // Classe Prodotto
+
 class Prodotto {
     
     private $nome;
@@ -11,12 +12,10 @@ class Prodotto {
     private $prezzo;
     private $immagine;
 
-    
+    use Sconto;
     
     public function __construct($nome, $descrizione, $prezzo, $immagine) {
         
-        
-
         $this->nome = $nome;
         $this->descrizione = $descrizione;
         $this->prezzo = $prezzo;
@@ -26,7 +25,9 @@ class Prodotto {
     public function visualizza_scheda_prodotto() {
         echo "Nome: " . $this->nome . "<br>";
         echo "Descrizione: " . $this->descrizione . "<br>";
-        echo "Prezzo: " . $this->prezzo . "<br>";
+        echo "Prezzo Pieno: " . $this->prezzo . "euro" . "<br>";
+        $this->applicaSconto();
+        echo "Prezzo scontato: " . $this->prezzo . "euro" . "<br>";
         echo "Immagine: <img src=''" . $this->immagine . "' width='300' height='300'><br>";
         echo "<br>";
     }
--- trait.php
+++ trait.php
@@ -1,2 +1,7 @@
 <?php
 
+trait Sconto {
+    public function applicaSconto() {
+        $this->prezzo *= 0.5;
+    }
+}
